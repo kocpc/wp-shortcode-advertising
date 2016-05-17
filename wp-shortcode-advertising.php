0aa0fa3562d799b6ff30b3c62a8797ed63045c5a
Hiram Huang <me@hiram.tw>
2016-05-18 00:28:28 +0800

    Create wp-shortcode-advertising.php
    Plugin main file.

---
+++ wp-shortcode-advertising.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * WP Shortcode Advertising
+ *
+ * Plugin Name: WP Shortcode Advertising
+ * Description: A plugin that allow you to insert advertising via shortcode.
+ * Author: Hiram Huang
+ * Author URI: https://www.facebook.com/naxqihao
+ * Version: 0.1
+ * Text Domain: wp-shortcode-advertising
+ * Domain Path: /languages
+ * License: MIT
+ * License URI: https://opensource.org/licenses/MIT
+ *
+ * @package		wp-shortcode-advertising
+ * @since		0.1
+ */
+
+if( ! defined( 'ABSPATH' ) ) {
+	die( 'SHAME ON YOU' );
+}
