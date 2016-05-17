b1359c8f876e1cfc305cef5214b0755d03eb1497
Hiram Huang <chown9835@gmail.com>
2016-05-17 16:40:47 +0000

    Create uninstall.php

---
+++ uninstall.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * WP Shortcode Advertising
+ * 
+ * @package wp-shortcode-advertising
+ * @since 0.1
+ */
+
+// If uninstall not called from WordPress, then exit.
+if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
+	exit;
+}
