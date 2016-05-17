5a98141b61cc4277db3c7d08c0f8e9418bc3a7cc
Hiram Huang <chown9835@gmail.com>
2016-05-17 17:15:10 +0000

    Insert settings link to plugin list.

--- settings/class-wp-shortcode-advertising-settings.php
+++ settings/class-wp-shortcode-advertising-settings.php
@@ -6,8 +6,41 @@
  * @since 0.1
  */
 
-class WP_Shortcode_Advertising {
+class WP_Shortcode_Advertising_Settings {
     
     const SA_PLUGIN_SETTINGS_SLUG = 'wp-shortcode-advertising-settings';
     
+    /**
+     * Initiator.
+     * 
+     * @since 0.1
+     */
+    public static function init() {
+        add_filter( 'plugin_action_links_' . SA_PLUGIN_PATH, array( 'WP_Shortcode_Advertising_Settings', 'add_settings_link_to_plugin_actions' ) );
+    }
+    
+    /**
+	 * Gets the URL/path for this page.
+	 * This function refer from facebook-instant-articles-wp.
+	 * 
+	 * @since 0.1
+	 */
+	public static function get_href_to_settings_page() {
+		return menu_page_url( self::SA_PLUGIN_SETTINGS_SLUG, false );
+	}
+    
+    /**
+	 * Creates an anchor element.
+	 * This function refer from facebook-instant-articles-wp.
+	 *
+	 * @param array $links The links will be added to anchor.
+	 * @since 0.1
+	 */
+	public static function add_settings_link_to_plugin_actions( $links ) {
+		$link_text = __( 'Settings' );
+		$settings_href = self::get_href_to_settings_page();
+		$settings_link = '<a href="' . esc_url( $settings_href ) . '">' . $link_text . '</a>';
+		array_push( $links, $settings_link );
+		return $links;
+	}
 }
