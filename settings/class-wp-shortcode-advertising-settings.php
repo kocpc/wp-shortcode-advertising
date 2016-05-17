ee4a6177a798d7e95b72e9705efd4bb89652d6be
Hiram Huang <chown9835@gmail.com>
2016-05-17 17:46:14 +0000

    Init administration meun, not functionally.

--- settings/class-wp-shortcode-advertising-settings.php
+++ settings/class-wp-shortcode-advertising-settings.php
@@ -16,6 +16,9 @@ class WP_Shortcode_Advertising_Settings {
      * @since 0.1
      */
     public static function init() {
+        // Initial administration meun.
+        add_action( 'admin_menu', array( 'WP_Shortcode_Advertising_Settings', 'menu_items' ) );
+        
         // Insert setting link to plugin list.
         add_filter( 'plugin_action_links_' . SA_PLUGIN_PATH, array( 'WP_Shortcode_Advertising_Settings', 'add_settings_link_to_plugin_actions' ) );
     }
@@ -43,5 +46,20 @@ class WP_Shortcode_Advertising_Settings {
 		$settings_link = '<a href="' . esc_url( $settings_href ) . '">' . $link_text . '</a>';
 		array_push( $links, $settings_link );
 		return $links;
+	}
+	
+	/**
+	 * Creates the menu items for FB Instant Article in WordPress side menu.
+	 * This function refer from facebook-instant-articles-wp.
+	 *
+	 * @since 0.1
+	 */
+	public static function menu_items() {
+		add_menu_page(
+			__( 'WP Shortcode Advertising Settings', 'wp-shortcode-advertising' ),
+			__( 'Shortcode AD', 'wp-shortcode-advertising' ),
+			'manage_options',
+			self::SA_PLUGIN_SETTINGS_SLUG
+		);
 	}
 }
