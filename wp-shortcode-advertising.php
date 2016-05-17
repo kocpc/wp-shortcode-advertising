2d3a03b26c72cc7794cd1acbf8d2f91acfadf808
Hiram Huang <chown9835@gmail.com>
2016-05-17 17:14:58 +0000

    Init plugin with settings class

--- wp-shortcode-advertising.php
+++ wp-shortcode-advertising.php
@@ -19,3 +19,23 @@
 if( ! defined( 'ABSPATH' ) ) {
 	die( 'SHAME ON YOU' );
 }
+
+/**
+ * Define plugin information.
+ * Part refer from facebook-instant-articles-wp.
+ */
+define( 'SA_PLUGIN_VERSION', '0.1' );
+define( 'SA_PLUGIN_PATH_FULL', __FILE__ );
+define( 'SA_PLUGIN_PATH', plugin_basename( __FILE__ ) );
+define( 'SA_PLUGIN_FILE_BASENAME', pathinfo( __FILE__, PATHINFO_FILENAME ) );
+define( 'SA_PLUGIN_TEXT_DOMAIN', 'wp-shortcode-advertising' );
+
+/**
+ * Import plugin classes.
+ */
+require_once( dirname( __FILE__ ) . '/settings/class-wp-shortcode-advertising-settings.php' );
+
+/**
+ * Initial plugin.
+ */
+WP_Shortcode_Advertising_Settings::init();
