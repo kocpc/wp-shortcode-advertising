061d06e658cad3febffea3851600f8a5137e076d
Hiram Huang <chown9835@gmail.com>
2016-05-17 16:53:47 +0000

    Create and init setting class

---
+++ settings/class-wp-shortcode-advertising-settings.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * WP Shortcode Advertising
+ * 
+ * @package wp-shortode-advertising
+ * @since 0.1
+ */
+
+class WP_Shortcode_Advertising {
+    
+    const SA_PLUGIN_SETTINGS_SLUG = 'wp-shortcode-advertising-settings';
+    
+}
