e1fbd03cc389a7376bf16c234d1632b30cdb2395
Hiram Huang <chown9835@gmail.com>
2016-05-22 07:02:52 +0000

    Add tablet code to show, not only mobile.

--- includes/class-wp-shortcode-advertising-filter.php
+++ includes/class-wp-shortcode-advertising-filter.php
@@ -44,15 +44,28 @@ class WP_Shortcode_Advertising_Filter {
         $post_id = get_the_ID();
         $post_author_id = get_post_field( 'post_author', $post_id );
         $advertising_code = [
-            'default' => get_the_author_meta( 'wpsa-default', $post_author_id ),
-            'mobile' => get_the_author_meta( 'wpsa-default-mobile', $post_author_id )
+            'default'   => get_the_author_meta( 'wpsa-default', $post_author_id ),
+            'mobile'    => get_the_author_meta( 'wpsa-default-mobile', $post_author_id ),
+            'tablet'    => get_the_author_meta( 'wpsa-default-tablet', $post_author_id )
             ];
         
+        // Import Mobile-Detect library
+        require_once( SA_PLUGIN_BASE_FULL . '/includes/mobile-detect.php' );
+        $mobile_detect = new Mobile_Detect();
+        
         // Rendering mobile advertising code if available.
-        if( $advertising_code['mobile'] && wp_is_mobile() ) {
+        if( $advertising_code['tablet'] && $mobile_detect->isTablet() ) {
+            // If is tablet device and code exist
+            return $advertising_code['tablet'];
+        } elseif( $advertising_code['mobile'] && $mobile_detect->isMobile() ) {
+            // If is mobile device or tablet code is not exist and mobile code exist
             return $advertising_code['mobile'];
-        } else {
+        } elseif( $advertising_code['default'] ) {
+            // Default code
             return $advertising_code['default'];
+        } else {
+            // No code exist, return blank.
+            return '';
         }
     }
 }
