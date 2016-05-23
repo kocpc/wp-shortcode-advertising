01db68248f1735a58575ca7305e28ffcd756c2db
Hiram Huang <chown9835@gmail.com>
2016-05-23 13:36:44 +0000

    Update textdomain to SA_PLUGIN_TEXT_DOMAIN

--- settings/template-profile-options.php
+++ settings/template-profile-options.php
@@ -7,11 +7,11 @@
  * @since   0.1
  */
 ?>
-<h2><?php _e( 'Advertising Code', 'wp-shortcode-advertising' ); ?></h2>
+<h2><?php _e( 'Advertising Code', SA_PLUGIN_TEXT_DOMAIN ); ?></h2>
 <table class="form-table">
 <tr>
     <th>
-        <label><?php _e( 'Default Advertising', 'wp-shortcode-advertising' ); ?></label>
+        <label><?php _e( 'Default Advertising', SA_PLUGIN_TEXT_DOMAIN ); ?></label>
     </th>
     <td>
         <textarea name="wpsa-default-advertising-code" rows="8" cols="30"><?php echo esc_html( $current_advertising['default'] ); ?></textarea>
@@ -19,18 +19,18 @@
 </tr>
 <tr>
     <th>
-        <label><?php _e( 'Mobile Advertising', 'wp-shortcode-advertising' ); ?></label>
+        <label><?php _e( 'Mobile Advertising', SA_PLUGIN_TEXT_DOMAIN ); ?></label>
     </th>
     <td>
-        <textarea name="wpsa-default-advertising-code-mobile" rows="8" cols="30" placeholder="<?php _e( '(Optional) If you fill in this field, mobile device will load this advertising code.', 'wp-shortcode-advertising'); ?>"><?php echo esc_html( $current_advertising['mobile'] ); ?></textarea>
+        <textarea name="wpsa-default-advertising-code-mobile" rows="8" cols="30" placeholder="<?php _e( '(Optional) If you fill in this field, mobile device will load this advertising code.', SA_PLUGIN_TEXT_DOMAIN ); ?>"><?php echo esc_html( $current_advertising['mobile'] ); ?></textarea>
     </td>
 </tr>
 <tr>
     <th>
-        <label><?php _e( 'Tablet Advertising', 'wp-shortcode-advertising' ); ?></label>
+        <label><?php _e( 'Tablet Advertising', SA_PLUGIN_TEXT_DOMAIN ); ?></label>
     </th>
     <td>
-        <textarea name="wpsa-default-advertising-code-tablet" rows="8" cols="30" placeholder="<?php _e( '(Optional) If you fill in this field, tablet device will load this advertising code.', 'wp-shortcode-advertising'); ?>"><?php echo esc_html( $current_advertising['tablet'] ); ?></textarea>
+        <textarea name="wpsa-default-advertising-code-tablet" rows="8" cols="30" placeholder="<?php _e( '(Optional) If you fill in this field, tablet device will load this advertising code.', SA_PLUGIN_TEXT_DOMAIN ); ?>"><?php echo esc_html( $current_advertising['tablet'] ); ?></textarea>
     </td>
 </tr>
 </table>
