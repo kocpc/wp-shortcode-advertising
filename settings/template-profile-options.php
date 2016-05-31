de1bd95af62ba7e00025cbd4d9a67d482bc5b2cf
Hiram Huang <chown9835@gmail.com>
2016-05-31 02:30:45 +0000

    Use index.php avoid web server list directory.

--- settings/template-profile-options.php
+++ settings/template-profile-options.php
@@ -7,8 +7,13 @@
  * @since   0.1
  */
  
-if( ! defined( 'ABSPATH' ) ) {
-	die( 'Do not direct access this file.' );
+/**
+ * If file is opened directly, return 403 error
+ */
+if( ! function_exists ('add_action') ) {
+	header( 'Status: 403 Forbidden' );
+	header( 'HTTP/1.1 403 Forbidden' );
+	exit();
 }
 ?>
 <h2><?php _e( 'Advertising Code', SA_PLUGIN_TEXT_DOMAIN ); ?></h2>
